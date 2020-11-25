2b0619742b3ac638b039e2a39ca7aef3c32c8768
vcoz17 <ntduc97@gmail.com>
2020-11-24 21:25:00 -0800

    add argv

--- test.py
+++ test.py
@@ -3,17 +3,17 @@ from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 import time
+import sys
 
 def main():
   # prompt email + password
-  email = str(input("email: "))
-  password = str(input("password: "))
+  f = open("accounts.txt","r")
 
   # open browser and login
   driver = webdriver.Chrome()
   driver.get("https://www.walmart.com/account/login")
-  driver.find_element_by_id("email").send_keys(email)
-  driver.find_element_by_id("password").send_keys(password)
+  driver.find_element_by_id("email").send_keys(str(f.readline()))
+  driver.find_element_by_id("password").send_keys(str(f.readline()))
   driver.find_element_by_xpath("//*[@id='sign-in-form']/button[1]").click()
 
   time.sleep(5)
@@ -30,6 +30,13 @@ def main():
   # buy 2 ps5
   Select(driver.find_element_by_xpath("//*[@id='add-on-atc-container']/div[1]/section/div[1]/div[2]/select")).select_by_visible_text('2')
   driver.find_element_by_xpath("//*[@id='add-on-atc-container']/div[1]/section/div[1]/div[3]/button/span").click()
+
+  time.sleep(3)
+  
+  # click checkout
+  driver.find_element_by_xpath("//*[@id='cart-root-container-content-skip']/div[1]/div/div[2]/div/div/div/div/div[3]/div/div/div[2]/div[1]/div[2]/div/button[1]").click()
+
+  time.sleep(3)
 
   while True:
     time.sleep(5)
