8b82578a848c561937998bc30ff6d0d381315a02
vcoz17 <ntduc97@gmail.com>
2020-11-25 17:44:13 -0800

    done

--- test.py
+++ test.py
@@ -21,15 +21,23 @@ def main(email, pwd, cvv):
   driver.find_element_by_id("password").send_keys(pwd)
   driver.find_element_by_xpath("//*[@id='sign-in-form']/button[1]").click()
 
-  time.sleep(5)
+  wait.until(EC.title_contains("Account"))
   actual_start = time.time()
   
-  driver.get("https://www.walmart.com/ip/Sony-PlayStation-4-500GB-Slim-System-Black/406966077")
+  driver.get("https://www.walmart.com/ip/PlayStation-5-Console/363472942")
+
+  add_to_cart_button = ''
+
+  while not add_to_cart_button:
+    time.sleep(0.8)
+    driver.get("https://www.walmart.com/ip/PlayStation-5-Console/363472942")
+    try:
+      add_to_cart_button = driver.find_element_by_css_selector("#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.prod-product-cta-add-to-cart.display-inline-block > button")
+      add_to_cart_button.click()
+    except:continue
 
   start = time.time()
-  wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.prod-product-cta-add-to-cart.display-inline-block > button")))
   time.sleep(random.uniform(0, 0.1))
-  driver.find_element_by_css_selector("#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.prod-product-cta-add-to-cart.display-inline-block > button").click()
   
   # click checkout
   wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#cart-root-container-content-skip > div:nth-child(1) > div > div.Cart-PACModal.standard-pac.pac-added.pac-new-ny.no-price-fulfillment.pac-vanilla-hf > div > div > div > div > div.Cart-PACModal-Body-right-rail.Grid-col.u-size-1.u-size-1-2-m.u-size-1-2-l > div > div > div.Grid-col.u-size-1-2.pos-actions-container > div.cart-pos-main-actions.s-margin-top > div.new-ny-styling.cart-pos-proceed-to-checkout > div > button.button.ios-primary-btn-touch-fix.hide-content-max-m.checkoutBtn.button--primary")))
@@ -66,6 +74,10 @@ def main(email, pwd, cvv):
   time.sleep(random.uniform(0, 0.1))
   driver.find_element_by_id("cvv-confirm").send_keys(cvv)
   driver.find_element_by_css_selector("body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div > div:nth-child(3) > div.CXO_module_container > div.CXO_module_body.ResponsiveContainer > div > div > div > div.text-left.Grid > div:nth-child(2) > div > button").click()
+  
+  # place order
+  wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div > div.Grid-col.u-size-1.u-size-3-12-s.u-size-3-12-m > div > form > div > button")))
+  driver.find_element_by_css_selector("body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div > div > div.Grid-col.u-size-1.u-size-3-12-s.u-size-3-12-m > div > form > div > button").click()
 
   print(time.time() - start)
   print("Total time: " + str(time.time() - actual_start))
