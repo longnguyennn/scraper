0e97cf93f5da7283eb93cc1ecadf04f69e5d4b1e
Long Nguyen <longnyen@amazon.com>
2020-11-25 16:49:23 -0800

    Input cvv and add delay between steps

--- test.py
+++ test.py
@@ -2,42 +2,43 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
-import time
-import sys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 import sys
+import time
+import random
 
-def main(email, pwd):
-
+def main(email, pwd, cvv):
   # open browser and login
-  driver = webdriver.Chrome()
-  wait = WebDriverWait(driver, 100)
+  options = Options()
+  options.page_load_strategy = 'eager'
+  driver = webdriver.Chrome(options=options)
+  wait = WebDriverWait(driver, 300)
   driver.get("https://www.walmart.com/account/login")
   driver.find_element_by_id("email").send_keys(email)
   driver.find_element_by_id("password").send_keys(pwd)
   driver.find_element_by_xpath("//*[@id='sign-in-form']/button[1]").click()
 
-  wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#main-content > span > div.hide-content-m > nav > h2")))
+  time.sleep(5)
+  actual_start = time.time()
   
   driver.get("https://www.walmart.com/ip/Sony-PlayStation-4-500GB-Slim-System-Black/406966077")
 
-  # buy 2 ps5
-  wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.display-inline-block.l-margin-right.QuantityPicker-select.font-semibold.field.field--select.field--secondary > select")))
   start = time.time()
-  Select(driver.find_element_by_css_selector("#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.display-inline-block.l-margin-right.QuantityPicker-select.font-semibold.field.field--select.field--secondary > select")).select_by_visible_text('2')
   wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, "#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.prod-product-cta-add-to-cart.display-inline-block > button")))
+  time.sleep(random.uniform(0, 0.1))
   driver.find_element_by_css_selector("#add-on-atc-container > div:nth-child(1) > section > div.valign-middle.display-inline-block.prod-product-primary-cta.primaryProductCTA-marker > div.prod-product-cta-add-to-cart.display-inline-block > button").click()
-
-  time.sleep(0.5)
   
   # click checkout
   wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "#cart-root-container-content-skip > div:nth-child(1) > div > div.Cart-PACModal.standard-pac.pac-added.pac-new-ny.no-price-fulfillment.pac-vanilla-hf > div > div > div > div > div.Cart-PACModal-Body-right-rail.Grid-col.u-size-1.u-size-1-2-m.u-size-1-2-l > div > div > div.Grid-col.u-size-1-2.pos-actions-container > div.cart-pos-main-actions.s-margin-top > div.new-ny-styling.cart-pos-proceed-to-checkout > div > button.button.ios-primary-btn-touch-fix.hide-content-max-m.checkoutBtn.button--primary")))
+  time.sleep(random.uniform(0, 0.1))
   driver.find_element_by_css_selector("#cart-root-container-content-skip > div:nth-child(1) > div > div.Cart-PACModal.standard-pac.pac-added.pac-new-ny.no-price-fulfillment.pac-vanilla-hf > div > div > div > div > div.Cart-PACModal-Body-right-rail.Grid-col.u-size-1.u-size-1-2-m.u-size-1-2-l > div > div > div.Grid-col.u-size-1-2.pos-actions-container > div.cart-pos-main-actions.s-margin-top > div.new-ny-styling.cart-pos-proceed-to-checkout > div > button.button.ios-primary-btn-touch-fix.hide-content-max-m.checkoutBtn.button--primary").click()
 
   # continue after checkout
   wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div > div:nth-child(1) > div > div.CXO_module_body.ResponsiveContainer > div > div > div > div.text-left.Grid > div > div > div.CXO_fulfillment_continue > button")))
+  time.sleep(random.uniform(0, 0.1))
   driver.find_element_by_css_selector("body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div > div:nth-child(1) > div > div.CXO_module_body.ResponsiveContainer > div > div > div > div.text-left.Grid > div > div > div.CXO_fulfillment_continue > button").click()
 
   # delivery address
@@ -57,15 +58,21 @@ def main(email, pwd):
 
   # continue after checkout
   wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, "body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div > div:nth-child(2) > div.CXO_module_container > div.CXO_module_body.ResponsiveContainer > div > div > div > div.text-left.Grid > div > div > div > div > div.arrange > div.arrange-fill.u-size-1-12-m > button")))
+  time.sleep(random.uniform(0, 0.1))
   driver.find_element_by_css_selector("body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div > div:nth-child(2) > div.CXO_module_container > div.CXO_module_body.ResponsiveContainer > div > div > div > div.text-left.Grid > div > div > div > div > div.arrange > div.arrange-fill.u-size-1-12-m > button").click()
 
+  # input CVV
+  wait.until(EC.presence_of_element_located((By.ID, "cvv-confirm")))
+  time.sleep(random.uniform(0, 0.1))
+  driver.find_element_by_id("cvv-confirm").send_keys(cvv)
+  driver.find_element_by_css_selector("body > div.js-content > div > div.checkout-wrapper > div > div.accordion-inner-wrapper > div.checkout-accordion > div > div > div > div:nth-child(3) > div.CXO_module_container > div.CXO_module_body.ResponsiveContainer > div > div > div > div.text-left.Grid > div:nth-child(2) > div > button").click()
+
   print(time.time() - start)
+  print("Total time: " + str(time.time() - actual_start))
 
   while True:
     time.sleep(5)
 
 if __name__ == "__main__":
-  email = sys.argv[1]
-  pwd = sys.argv[2]
-  print(email, pwd)
-  main(email, pwd)
+  email, pwd, cvv = sys.argv[1], sys.argv[2], sys.argv[3]
+  main(email, pwd, cvv)
